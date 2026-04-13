6a154d673973c13d60e94e4b557376344a8ed56c
kappasan21 <191061113+ks-account@users.noreply.github.com>
2026-04-13 12:17:33 +0900

    Release before remove all test cases which is not a final version

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,34 +1,34 @@
-// const allowedOrigins = [
-//     process.env.SELF_URL,
-//     process.env.RECIPE_CLIENT_URL,
-//     process.env.TASK_MGT_APP_URL,
-//     process.env.CV_APP_URL,
-//     process.env.CODEY_CLIENT_URL,
-//     process.env.MUSIC_SEARCH_CLIENT_URL,
-//     process.env.CHINESE_REST_CLIENT_URL,
-//     process.env.HEALTH_CHECK_APP_URL,
+// const { options } = require("../app")
 
-const { options } = require("../app")
-
-// ];
-const SELF_URL = 'https://auth-server-2-1.onrender.com'
-const RECIPE_CLIENT_URL = 'https://recipe-app-client-g01-0-9.netlify.app'
-const TASK_MGT_APP_URL = 'https://task-appoint-app-g01-0-9.netlify.app'
-const CV_APP_URL = 'https://cv-app-g01-0-9.onrender.com'
-const CODEY_CLIENT_URL = 'https://codey-store-client-g01-0-9.netlify.app'
-const MUSIC_SEARCH_CLIENT_URL = 'https://music-mgt-app-g01-0-9.netlify.app'
-const CHINESE_REST_CLIENT_URL = 'https://restaurant-app-client-g01-0-9.netlify.app'
-const HEALTH_CHECK_APP_URL = 'https://polite-semolina-a97790.netlify.app'
 const allowedOrigins = [
-    SELF_URL,
-    RECIPE_CLIENT_URL,
-    TASK_MGT_APP_URL,
-    CV_APP_URL,
-    CODEY_CLIENT_URL,
-    MUSIC_SEARCH_CLIENT_URL,
-    CHINESE_REST_CLIENT_URL,
-    HEALTH_CHECK_APP_URL,
+    process.env.SELF_URL,
+    process.env.RECIPE_CLIENT_URL,
+    process.env.TASK_MGT_APP_URL,
+    process.env.CV_APP_URL,
+    process.env.CODEY_CLIENT_URL,
+    process.env.MUSIC_SEARCH_CLIENT_URL,
+    process.env.CHINESE_REST_CLIENT_URL,
+    process.env.HEALTH_CHECK_APP_URL,
 ];
+
+// const SELF_URL = 'https://auth-server-2-1.onrender.com'
+// const RECIPE_CLIENT_URL = 'https://recipe-app-client-g01-0-9.netlify.app'
+// const TASK_MGT_APP_URL = 'https://task-appoint-app-g01-0-9.netlify.app'
+// const CV_APP_URL = 'https://cv-app-g01-0-9.onrender.com'
+// const CODEY_CLIENT_URL = 'https://codey-store-client-g01-0-9.netlify.app'
+// const MUSIC_SEARCH_CLIENT_URL = 'https://music-mgt-app-g01-0-9.netlify.app'
+// const CHINESE_REST_CLIENT_URL = 'https://restaurant-app-client-g01-0-9.netlify.app'
+// const HEALTH_CHECK_APP_URL = 'https://polite-semolina-a97790.netlify.app'
+// const allowedOrigins = [
+//     SELF_URL,
+//     RECIPE_CLIENT_URL,
+//     TASK_MGT_APP_URL,
+//     CV_APP_URL,
+//     CODEY_CLIENT_URL,
+//     MUSIC_SEARCH_CLIENT_URL,
+//     CHINESE_REST_CLIENT_URL,
+//     HEALTH_CHECK_APP_URL,
+// ];
 
 const corsOptions = {
     origin: function (origin, callback) {
@@ -39,7 +39,7 @@ const corsOptions = {
             return callback(null, true);
         }
 
-        // Check if the request origin is in the allowedOrigins list
+        //
         if (allowedOrigins.includes(origin)) {
             callback(null, true);
         } else {
